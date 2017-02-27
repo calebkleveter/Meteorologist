7c3826fa5407cac0060efd218949725117de7cc2
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-27 12:44:25 -0600

    Meteorologist/WeatherTableView.swift - Created WeatherTableView.tempHighIcon label

--- Meteorologist/WeatherTableView.swift
+++ Meteorologist/WeatherTableView.swift
@@ -9,5 +9,9 @@
 import UIKit
 
 class WeatherTableView: UIView {
-    
+    lazy var tempHighIcon: UILabel = {
+        let label = UILabel()
+        WeatherIcon().set(label, textTo: .up, with: 36)
+        return label
+    }()
 }
