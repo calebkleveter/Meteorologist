dfd65e43a45052c056755d206d285a952e1bd65b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-27 12:31:09 -0600

    Meterologist/WeatherTableView.swift - Created file

---
+++ Meteorologist/WeatherTableView.swift
@@ -0,0 +1,11 @@
+//
+//  WeatherTableView.swift
+//  Meteorologist
+//
+//  Created by Caleb Kleveter on 2/27/17.
+//  Copyright © 2017 Caleb Kleveter. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableView: UIView {}
