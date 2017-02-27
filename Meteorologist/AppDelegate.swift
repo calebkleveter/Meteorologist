d683bef7bc63a0cf7123ed23428154b72246da3c
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-27 12:23:47 -0600

    Meterologist/AppDelegate.swift - Added manual app creation

--- Meteorologist/AppDelegate.swift
+++ Meteorologist/AppDelegate.swift
@@ -15,7 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = window else { return false }
+        
+        window.backgroundColor = .white
+        
+        let weatherTableController = WeatherTableController(style: .plain)
+        let navigationController = UINavigationController(rootViewController: weatherTableController)
+        
+        window.rootViewController = navigationController
+        
+        window.makeKeyAndVisible()
+        
         return true
     }
 
