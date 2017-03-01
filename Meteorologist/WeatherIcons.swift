b599859317c589857625c345199af1ba9c64f694
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-01 17:31:38 -0600

    Meteorologist/WeatherIcons.swift - Fixed hex values

--- Meteorologist/WeatherIcons.swift
+++ Meteorologist/WeatherIcons.swift
@@ -37,21 +37,21 @@ struct WeatherIcon {
         
         var hex: String {
             switch self {
-            case .clearDay: return "\u{00d}"
-            case .clearNight: return "\u{02e}"
-            case .rain: return "\u{019}"
-            case .snow: return "\u{01b}"
-            case .sleet: return "\u{0b5}"
-            case .wind: return "\u{050}"
-            case .fog: return "\u{014}"
-            case .cloudy: return "\u{013}"
-            case .partlyCloudyDay: return "\u{002}"
-            case .partlyCloudyNight: return "\u{086}"
-            case .unSupported: return "\u{07b}"
-            case .up: return "\u{058}"
-            case .down: return "\u{044}"
-            case .humidity: return "\u{07a}"
-            case .precipitation: return "\u{04e}"
+            case .clearDay: return "\u{f00d}"
+            case .clearNight: return "\u{f02e}"
+            case .rain: return "\u{f019}"
+            case .snow: return "\u{f01b}"
+            case .sleet: return "\u{f0b5}"
+            case .wind: return "\u{f050}"
+            case .fog: return "\u{f014}"
+            case .cloudy: return "\u{f013}"
+            case .partlyCloudyDay: return "\u{f002}"
+            case .partlyCloudyNight: return "\u{f086}"
+            case .unSupported: return "\u{f07b}"
+            case .up: return "\u{f058}"
+            case .down: return "\u{f044}"
+            case .humidity: return "\u{f07a}"
+            case .precipitation: return "\u{f04e}"
             }
         }
     }
