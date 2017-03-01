86d344a90ef5a03a8ecabda9da25c3cc06a82181
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-01 16:32:37 -0600

    Meteorologist/WeatherTableView.swift - Created inits and configureLayout()

--- Meteorologist/WeatherTableView.swift
+++ Meteorologist/WeatherTableView.swift
@@ -14,4 +14,26 @@ class WeatherTableView: UIView {
         WeatherIcon.set(label, textTo: .up, with: 36)
         return label
     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureLayout() {
+        self.addSubview(tempHighIcon)
+        
+        tempHighIcon.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+                NSLayoutConstraint(item: tempHighIcon, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 15),
+                NSLayoutConstraint(item: tempHighIcon, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 5),
+                tempHighIcon.heightAnchor.constraint(equalToConstant: 15),
+                tempHighIcon.widthAnchor.constraint(equalToConstant: 10)
+            ])
+    }
 }
