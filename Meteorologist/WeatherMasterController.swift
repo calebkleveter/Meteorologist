95bea5b3d4d4ab5f66b56da6b1e18d1c7900adb9
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-02 16:02:25 -0600

    Meteorologist/WeatherMasterController.swift - Created file

---
+++ Meteorologist/WeatherMasterController.swift
@@ -0,0 +1,35 @@
+//
+//  WeatherMasterController.swift
+//  Meteorologist
+//
+//  Created by Caleb Kleveter on 3/2/17.
+//  Copyright © 2017 Caleb Kleveter. All rights reserved.
+//
+
+import UIKit
+
+class WeatherMasterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
