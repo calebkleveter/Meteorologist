a6526669cac3dfcc544765c2654666786a51ab67
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-02 08:13:39 -0600

    Meteorologist/WeatherTableView.swift - Added constraints to  WeatherTableView.tempLowIcon

--- Meteorologist/WeatherTableView.swift
+++ Meteorologist/WeatherTableView.swift
@@ -43,9 +43,11 @@ class WeatherTableView: UIView {
     func configureLayout() {
         self.addSubview(tempHighIcon)
         self.addSubview(tempHighLabel)
+        self.addSubview(tempLowIcon)
         
         tempHighIcon.translatesAutoresizingMaskIntoConstraints = false
         tempHighLabel.translatesAutoresizingMaskIntoConstraints = false
+        tempLowIcon.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
                 NSLayoutConstraint(item: tempHighIcon, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 20),
@@ -56,7 +58,12 @@ class WeatherTableView: UIView {
                 NSLayoutConstraint(item: tempHighLabel, attribute: .left, relatedBy: .equal, toItem: tempHighIcon, attribute: .right, multiplier: 1.0, constant: 10),
                 NSLayoutConstraint(item: tempHighLabel, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 20),
                 tempHighLabel.heightAnchor.constraint(equalToConstant: 20),
-                tempHighLabel.widthAnchor.constraint(equalToConstant: 50)
+                tempHighLabel.widthAnchor.constraint(equalToConstant: 50),
+                
+                NSLayoutConstraint(item: tempLowIcon, attribute: .top, relatedBy: .equal, toItem: tempHighIcon, attribute: .bottom, multiplier: 1.0, constant: 10),
+                NSLayoutConstraint(item: tempLowIcon, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 15),
+                tempLowIcon.heightAnchor.constraint(equalToConstant: 20),
+                tempLowIcon.widthAnchor.constraint(equalToConstant: 14)
             ])
     }
 }
