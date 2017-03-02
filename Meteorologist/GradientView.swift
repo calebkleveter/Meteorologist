0f21e81786bf25bd6d175758f681fef850e6dcf6
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-02 15:27:36 -0600

    Meteorologist/GradientView.swift - Created file

---
+++ Meteorologist/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Meteorologist
+//
+//  Created by Caleb Kleveter on 3/2/17.
+//  Copyright © 2017 Caleb Kleveter. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
