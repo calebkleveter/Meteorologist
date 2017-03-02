a33c0f13164856b9e447b1da3c6244109d90e88a
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-02 15:32:19 -0600

    Meteorologist/GradientView.swift - Created initilizer

--- Meteorologist/GradientView.swift
+++ Meteorologist/GradientView.swift
@@ -18,4 +18,21 @@ class GradientView: UIView {
     }
     */
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let blue = UIColor(red: 18/255, green: 7/255, blue: 161/255, alpha: 1)
+        let green = UIColor(red: 0/255, green: 212/255, blue: 42/255, alpha: 1)
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = self.frame
+        gradient.colors = [green.cgColor, blue.cgColor]
+        gradient.locations = [0.0, 1.0]
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
