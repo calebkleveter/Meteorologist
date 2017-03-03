8b0bcb3b26d37e832bba512f8483c8583b1704bd
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-03 11:41:38 -0600

    Meteorologist/JSONFetcher.swift - Created JSONFetcher.getJSON(urlString completion)

--- Meteorologist/JSONFetcher.swift
+++ Meteorologist/JSONFetcher.swift
@@ -17,4 +17,24 @@ struct JSONFetcher: APIClient {
         self.configuration = URLSessionConfiguration.default
         self.session = URLSession(configuration: self.configuration)
     }
+    
+    func getJSON(from urlString: String, with completion: @escaping (CurrentWeather, [DailyWeather]) -> ()) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            dataTask(with: request, endingWith: { (json, response, error) in
+                guard let json = json else {
+                    print("Data Task Error: ", error ?? "No error")
+                    return
+                }
+                let currentWeather = CurrentWeather(json: json)
+                let dailyData = ((json["daily"] as? JSON)?["data"] as? [JSON])
+                if let dailyWeather = dailyData?.map({ return DailyWeather(json: $0) }) {
+                    completion(currentWeather, dailyWeather)
+                    print("Current: ", currentWeather)
+                    print("Daily: ", dailyWeather)
+                }
+                
+            }).resume()
+        }
+    }
 }
