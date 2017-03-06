ed2d5ec23783168465390cfa824c05c9ec37070b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-06 16:48:50 -0600

    Meteorologist/DetailWeatherView.swift - Created file

---
+++ Meteorologist/DetailWeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailWeatherView.swift
+//  Meteorologist
+//
+//  Created by Caleb Kleveter on 3/6/17.
+//  Copyright © 2017 Caleb Kleveter. All rights reserved.
+//
+
+import UIKit
+
+class DetailWeatherView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
