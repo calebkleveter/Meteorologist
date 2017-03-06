c79719b9ef019b45d37d002d4535c1df323a44af
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-06 16:15:47 -0600

    Meteorologist/DailyWeatherCell.swift - Created DailyWeatherCell.init

--- Meteorologist/DailyWeatherCell.swift
+++ Meteorologist/DailyWeatherCell.swift
@@ -31,6 +31,16 @@ class DailyWeatherCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.configureLayout()
+        self.backgroundColor = .clear
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func configureLayout() {
         self.addSubview(dateLabel)
         
