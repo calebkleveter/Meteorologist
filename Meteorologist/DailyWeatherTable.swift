ed219635b94866b0b0261d719d58078a240f429b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-06 15:50:24 -0600

    Meteorologist/DailyWeatherTabel.swift - Created inits

--- Meteorologist/DailyWeatherTable.swift
+++ Meteorologist/DailyWeatherTable.swift
@@ -9,13 +9,12 @@
 import UIKit
 
 class DailyWeatherTable: UITableView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect, style: UITableViewStyle) {
+        super.init(frame: frame, style: style)
+        self.backgroundColor = .clear
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
