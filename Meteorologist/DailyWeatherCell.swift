e94f6c94bc1e4c51d59114bd8ebd0ce4bd757939
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-06 15:48:44 -0600

    Meteorologist/DailyWeatherCell.swift - Created DailyWeatherCell.configureLayout()

--- Meteorologist/DailyWeatherCell.swift
+++ Meteorologist/DailyWeatherCell.swift
@@ -31,4 +31,15 @@ class DailyWeatherCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    func configureLayout() {
+        self.addSubview(dateLabel)
+        
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+                NSLayoutConstraint(item: dateLabel, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 10),
+                dateLabel.widthAnchor.constraint(equalToConstant: 50)
+            ])
+    }
+    
 }
