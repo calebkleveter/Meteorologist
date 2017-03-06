a959f2ed033f5d555933a470f486d687bb9d5550
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-06 15:43:05 -0600

    Meteorologist/Date.swift - Created Date.abbreviatedDate(advance)

--- Meteorologist/Date.swift
+++ Meteorologist/Date.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+extension Date {
+    static func abbreviatedDate(advancedBy advance: Int = 0) -> String {
+        let date = Date()
+        let dateFomatter = DateFormatter()
+        dateFomatter.dateFormat = "MMM d"
+        var formattedDate = ""
+        var loopCounter = 0
+        let formattedDateChars = dateFomatter.string(from: date).characters
+        for char in dateFomatter.string(from: date).characters {
+            if loopCounter == formattedDateChars.count - 1 {
+                formattedDate += String(Int(String(char))! + advance)
+            } else { formattedDate += String(char) }
+            loopCounter += 1
+        }
+        return formattedDate
+    }
+}
