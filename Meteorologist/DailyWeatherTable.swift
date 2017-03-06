9437279d10eafc3240fdb8a766a4d6a1d2715485
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-06 15:49:25 -0600

    Meteorologist/DailyWeatherTabel.swift - Created file

---
+++ Meteorologist/DailyWeatherTable.swift
@@ -0,0 +1,21 @@
+//
+//  DailyWeatherTable.swift
+//  Meteorologist
+//
+//  Created by Caleb Kleveter on 3/6/17.
+//  Copyright © 2017 Caleb Kleveter. All rights reserved.
+//
+
+import UIKit
+
+class DailyWeatherTable: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
