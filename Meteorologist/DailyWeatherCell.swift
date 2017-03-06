c8fe02264072b7acddc62996986eed18dee6e576
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-06 15:45:37 -0600

    Meteorologist/DailyWeatherCell.swift - Created DailyWeatherCell.dateLabel

--- Meteorologist/DailyWeatherCell.swift
+++ Meteorologist/DailyWeatherCell.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class DailyWeatherCell: UITableViewCell {
 
+    lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "HelveticaNeue-Light", size: 30)
+        label.textColor = .white
+        label.backgroundColor = .clear
+        // FIXME: - This is a placeholder
+        label.text = "Oct 23"
+        return label
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
