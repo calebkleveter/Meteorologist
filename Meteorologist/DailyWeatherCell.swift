d3f35a1abb44b2d11bb842a47cbd05a0142cb5b1
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-06 15:06:55 -0600

    Meteorologist/DailyWeatherCell - Created file

---
+++ Meteorologist/DailyWeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  DailyWeatherCell.swift
+//  Meteorologist
+//
+//  Created by Caleb Kleveter on 3/6/17.
+//  Copyright © 2017 Caleb Kleveter. All rights reserved.
+//
+
+import UIKit
+
+class DailyWeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
