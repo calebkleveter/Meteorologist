9a0ece11eddf9cfaf8dc8a54483ae4b1ab96222a
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-17 15:56:23 -0500

    Meteorologist/WeatherDetailView.swift - Set weatherView's data to weather data

--- Meteorologist/WeatherDetailController.swift
+++ Meteorologist/WeatherDetailController.swift
@@ -16,6 +16,7 @@ class WeatherDetailController: UIViewController {
     init(with weatherData: DailyWeather) {
         self.weatherData = weatherData
         super.init(nibName: nil, bundle: nil)
+        weatherView.reloadData(with: weatherData)
         weatherView.frame = view.frame
         self.view = weatherView
     }
@@ -26,7 +27,6 @@ class WeatherDetailController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
     }
 
