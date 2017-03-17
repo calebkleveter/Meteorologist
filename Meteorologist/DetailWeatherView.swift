2d7c0b6d0809d58f97f25a618a95da523b1cc808
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-17 13:11:39 -0500

    Meteorologist/DetailWeatherView.swift - Created DetailWeatherView.weatherIcon

--- Meteorologist/DetailWeatherView.swift
+++ Meteorologist/DetailWeatherView.swift
@@ -9,13 +9,9 @@
 import UIKit
 
 class DetailWeatherView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    lazy var weatherIcon: UILabel = {
+        let label = UILabel()
+        WeatherIcon.set(label, textTo: .unSupported, with: 76)
+        return label
+    }()
 }
