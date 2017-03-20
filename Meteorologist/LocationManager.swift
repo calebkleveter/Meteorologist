d7f49112d2016454fd04380ef42bc84f17476af6
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-20 15:54:30 -0500

    Meteorologist/LocationManager.swift - Finished delegate implementation

--- Meteorologist/LocationManager.swift
+++ Meteorologist/LocationManager.swift
@@ -11,10 +11,12 @@ import CoreLocation
 
 class LocationManager: NSObject {
     let locationManager = CLLocationManager()
+    var onLocationUpdate: ((CLLocation) -> Void)?
     
     override init() {
         super.init()
         self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
     }
     
     func getUserPermission() {
@@ -26,4 +28,20 @@ class LocationManager: NSObject {
 
 extension LocationManager: CLLocationManagerDelegate {
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            manager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first,
+            let onLocationUpdate = onLocationUpdate {
+                onLocationUpdate(location)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
 }
