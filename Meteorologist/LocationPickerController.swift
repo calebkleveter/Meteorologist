905ed6d5c216ec4d6ae1c6ce32c54b44db8ad786
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-21 10:19:36 -0500

    Meteorologist/LocationPickerController.swift - Added inits. Moved view init to viewDidLoad

--- Meteorologist/LocationPickerController.swift
+++ Meteorologist/LocationPickerController.swift
@@ -12,17 +12,23 @@ class LocationPickerController: UIViewController {
 
     var pickerView: LocationPickerView? = nil
     
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pickerView = LocationPickerView(frame: self.view.frame)
+        self.view.addSubview(pickerView!)
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        pickerView = LocationPickerView(frame: self.view.frame)
-        guard let view = pickerView else { fatalError("View init failed") }
-        self.view = view
+        print(view)
     }
 
 }
