fd28cb9dd41f1bf67e26daa48ac7974a98f8b3e1
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-21 08:50:32 -0500

    Meteorologist/LocationPickerView.swift - Created file

---
+++ Meteorologist/LocationPickerView.swift
@@ -0,0 +1,21 @@
+//
+//  LocationPickerView.swift
+//  Meteorologist
+//
+//  Created by Caleb Kleveter on 3/21/17.
+//  Copyright © 2017 Caleb Kleveter. All rights reserved.
+//
+
+import UIKit
+
+class LocationPickerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
