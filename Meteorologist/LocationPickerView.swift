5fbf910628945090274ef0f5d7cffe893b8aaccc
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-21 08:50:56 -0500

    Meteorologist/LocationPickerView.swift - Imported MapKit

--- Meteorologist/LocationPickerView.swift
+++ Meteorologist/LocationPickerView.swift
@@ -7,15 +7,6 @@
 //
 
 import UIKit
+import MapKit
 
-class LocationPickerView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
-}
+class LocationPickerView: UIView {}
