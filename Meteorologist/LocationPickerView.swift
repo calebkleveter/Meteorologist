55ad0da0c8410ebf35f21df4ad95042dd437b335
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-21 09:44:05 -0500

    Meteorologist/LocationPickView.swift - Created LocationPickerView.configureLayout()

--- Meteorologist/LocationPickerView.swift
+++ Meteorologist/LocationPickerView.swift
@@ -45,4 +45,44 @@ class LocationPickerView: UIView {
         button.isUserInteractionEnabled = true
         return button
     }()
+    
+    func configureLayout() {
+        self.addSubview(mapView)
+        self.addSubview(useCurrentLocationButton)
+        self.addSubview(selectLocationButton)
+        self.addSubview(searchBar)
+        self.addSubview(locationSearchResultsTable)
+        
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        locationSearchResultsTable.translatesAutoresizingMaskIntoConstraints = false
+        selectLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        useCurrentLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+                NSLayoutConstraint(item: mapView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: mapView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: mapView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0),
+                mapView.heightAnchor.constraint(equalToConstant: 200),
+                
+                NSLayoutConstraint(item: searchBar, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: searchBar, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: searchBar, attribute: .top, relatedBy: .equal, toItem: mapView, attribute: .bottom, multiplier: 1.0, constant: 0),
+                
+                NSLayoutConstraint(item: locationSearchResultsTable, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: locationSearchResultsTable, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: locationSearchResultsTable, attribute: .top, relatedBy: .equal, toItem: searchBar, attribute: .bottom, multiplier: 1.0, constant: 0),
+                locationSearchResultsTable.heightAnchor.constraint(equalToConstant: 200 - searchBar.frame.size.height - 40),
+                
+                NSLayoutConstraint(item: selectLocationButton, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: selectLocationButton, attribute: .top, relatedBy: .equal, toItem: locationSearchResultsTable, attribute: .bottom, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: selectLocationButton, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0),
+                selectLocationButton.widthAnchor.constraint(equalToConstant: self.frame.size.width / 2),
+                
+                NSLayoutConstraint(item: useCurrentLocationButton, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: useCurrentLocationButton, attribute: .top, relatedBy: .equal, toItem: locationSearchResultsTable, attribute: .bottom, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: useCurrentLocationButton, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0),
+                useCurrentLocationButton.widthAnchor.constraint(equalToConstant: self.frame.size.width / 2),
+            ])
+    }
 }
